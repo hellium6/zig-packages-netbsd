354347067f8706038164089beb92ef5d72b8503c
hellium6 <88252053+hellium6@users.noreply.github.com>
2025-07-05 17:53:54 +0600

    Add zig 0.13.0 packages

---
+++ pkgsrc/devel/zls-0.13.0/zig-depends.mk
@@ -0,0 +1,5 @@
+# $NetBSD: zig-depends.mk,v 1.1 2024/04/29 13:27:52 nikita Exp $
+
+## Updated from ${WRKDIR}/zls-0.13.0/build.zig.zon
+ZIG_PACKAGE_DEPENDS+=	https://github.com/ziglibs/known-folders/archive/0ad514dcfb7525e32ae349b9acc0a53976f3a9fa.tar.gz
+ZIG_PACKAGE_DEPENDS+=	https://github.com/ziglibs/diffz/archive/ef45c00d655e5e40faf35afbbde81a1fa5ed7ffb.tar.gz
